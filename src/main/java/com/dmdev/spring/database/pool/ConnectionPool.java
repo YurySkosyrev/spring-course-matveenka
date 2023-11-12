2f7fb6c652b19a89640ddb711602b4f49693e8c0
YurySkosyrev <skosyrev-87@mail.ru>
2023-11-12 19:23:34 +0300

    Add context:component-scan
    - inject with constructor
    - @Value annotation
    - add @Component, @Service, @Repository

--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -1,25 +1,22 @@
 package com.dmdev.spring.database.pool;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Map;
 
+@Component("pool1")
 public class ConnectionPool {
     private final String name;
     private final Integer poolSize;
-    private final List<Object> args;
-    private Map<String, Object> properties;
 
-    public ConnectionPool(String name, Integer poolSize, List<Object> args, Map<String, Object> properties) {
+
+    public ConnectionPool(@Value("${db.username}") String name, @Value("${db.pool.size}") Integer poolSize) {
         this.name = name;
         this.poolSize = poolSize;
-        this.args = args;
-        this.properties = properties;
-    }
-
-    public void setProperties(Map<String, Object> properties) {
-        this.properties = properties;
     }
 
     @PostConstruct
--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -4,15 +4,29 @@ import com.dmdev.spring.bpp.InjectBean;
 import com.dmdev.spring.bpp.Transaction;
 import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.entity.Company;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 @Transaction
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+    private final ConnectionPool pool1;
+    private final List<ConnectionPool> pools;
+    private final Integer poolSize;
+
+    public CompanyRepository(ConnectionPool pool1,
+                             List<ConnectionPool> pools,
+                             @Value("${db.pool.size}")
+                             Integer poolSize) {
+        this.pool1 = pool1;
+        this.pools = pools;
+        this.poolSize = poolSize;
+    }
 
     @PostConstruct
     private void init() {
