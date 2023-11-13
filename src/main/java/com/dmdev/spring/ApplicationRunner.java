18032c7d9a7c7239cd191643a2a4908fa0ff135e
YurySkosyrev <skosyrev-87@mail.ru>
2023-11-13 11:30:33 +0300

    Replace ClassPathXmlApplicationContext with AnnotationConfigApplicationContext
    - set @PropertySource("classpath:application.properties")
    - set @ComponentScan(basePackages...)
    - add custom @ComponentScan.Filter

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,12 +1,15 @@
 package com.dmdev.spring;
 
+import com.dmdev.spring.config.ApplicationConfiguration;
 import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.database.repository.CrudRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
             CrudRepository  companyRepository = context.getBean("companyRepository", CrudRepository.class);
             System.out.println(companyRepository.findById(1));
         }
