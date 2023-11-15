ae26e133627a9d6da2db37632d93de73bc99a43d
YurySkosyrev <skosyrev-87@mail.ru>
2023-11-15 09:25:34 +0300

    Start to refactor to SpringBootApplication

---
+++ src/main/java/com/dmdev/spring/config/JPAConfiguration.java
@@ -0,0 +1,17 @@
+package com.dmdev.spring.config;
+
+import com.dmdev.spring.config.condition.JPACondition;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Conditional(JPACondition.class)
+@Configuration
+public class JPAConfiguration {
+
+    @PostConstruct
+    public void init(){
+        System.out.println("JPA configuration is enabled");
+    }
+}
